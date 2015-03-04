80e040f67a2bd522093de7b92001bcf01db1bb62
Viki Harrod <victoria.harrod@gmail.com>
2015-03-04 13:40:41 -0700

    be explicit about which routes we want

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :users
-      resources :trips
+      resources :users, except: [:index, :new, :edit]
+      resources :trips, except: [:new, :edit]
     end
   end
   # The priority is based upon order of creation: first created -> highest priority.
