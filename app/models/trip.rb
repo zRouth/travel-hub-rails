d0821cd59b60e7bddb6512284a393cabf6deeeab
zRouth <zrouthier@gmail.com>
2015-03-04 13:59:31 -0700

    add trip accounts validations and spec

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,5 +1,6 @@
 class Trip < ActiveRecord::Base
   belongs_to :user
+  has_many :trip_accounts
 
   validates :name, :start_date, :end_date, :user, presence: true
 end
---
+++ spec/factories/trip_accounts.rb
@@ -0,0 +1,5 @@
+FactoryGirl.define do
+  factory :trip_account do
+    trip
+  end
+end
