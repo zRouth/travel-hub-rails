cf57ddc108b29559fa4460e88e0bec271d16a35d
Viki Harrod <victoria.harrod@gmail.com>
2015-03-04 11:43:00 -0700

    kill users index, we don't need it

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -10,29 +10,6 @@ RSpec.describe Api::V1::UsersController, :type => :controller do
     expect(user_data["email"]).to eq("worace@example.com")
   end
 
-  it "fetches an index" do
-    user = create(:user, name: "worace", email: "worace@example.com")
-
-    create(:user, name: "mee")
-
-    get :index
-    users_data = JSON.parse(@response.body)
-    assert_equal ["users"], users_data.keys
-    assert_equal 2, users_data["users"].length
-    assert_equal "worace", users_data["users"].first["name"]
-    assert_equal "mee", users_data["users"].last["name"]
-  end
-
-  it "fetches a specified batch of users on the index" do
-    create(:user, name: "worace", email: "worace@example.com")
-    torta  = create(:user, name: "herbs", email: "torta@example.com")
-    zrouth = create(:user, name: "zach", email: "zrouth@example.com")
-
-    get :index, ids: [torta.id, zrouth.id]
-    data = JSON.parse(@response.body)
-    assert_equal [torta.id, zrouth.id], data["users"].map { |u| u["id"] }
-  end
-
   it "updates a user's info" do
     user = create(:user)
 
